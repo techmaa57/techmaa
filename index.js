33e4d94d375b1ab71aa7bc4e0c79dbd9de75af29
Mudit Jain <muditj148@gmail.com>
2022-12-28 00:13:11 +0530

    Small Fixes

--- index.js
+++ index.js
@@ -15,6 +15,40 @@ function setLanguage(){
 
 window.onload = setLanguage
 
+window.onscroll = function () {
+
+    if(document.documentElement.scrollTop > 20){
+        for(let i = 0; i < 3; i++){
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.add('fixed')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.add('w-full')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.remove('container')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.remove('py-2')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.add('bg-white')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.add('text-black')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.add('z-[100]')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.add('drop-shadow-md')
+        }
+        for(let i = 0; i < 12; i++){
+            if(document.querySelectorAll('nav .text-gray-200')[i]) document.querySelectorAll('nav .text-gray-200')[i].classList.add('text-gray-800')
+        }
+    } else {
+        for(let i = 0; i < 3; i++){
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.remove('fixed')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.remove('w-full')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.add('container')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.add('py-2')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.remove('bg-white')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.remove('z-[100]')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.remove('drop-shadow-md')
+            document.querySelectorAll('nav:not([aria-label="Breadcrumb"])')[i].classList.remove('text-black')
+        }
+        for(let i = 0; i < 12; i++){
+            if(document.querySelectorAll('nav .text-gray-800')[i]) document.querySelectorAll('nav .text-gray-800')[i].classList.remove('text-gray-800')
+        }
+    }
+
+}
+
 function faq(event) {
     var node = event.target
     if(node.tagName === 'path') node = node.parentNode
@@ -25,7 +59,7 @@ function faq(event) {
 }
 
 function search(lang){
-    window.location = "http://127.0.0.1:5500/store.html?" + document.getElementById(`search-navbar-${lang}`).value.toUpperCase();
+    window.location = "https://techmaa57.com/store.html?" + document.getElementById(`search-navbar-${lang}`).value.toUpperCase();
 }
 
 
